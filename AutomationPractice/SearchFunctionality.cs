3961379bd69f74ee175aaf0407f964e63b642ee7
DieroKreator <qtechwired@gmail.com>
2025-02-05 08:19:02 -0300

    Refactor search functionality test to use a variable for search string and enhance assertion message

--- AutomationPractice/SearchFunctionality.cs
+++ AutomationPractice/SearchFunctionality.cs
@@ -15,10 +15,14 @@ public class SearchFunctionality
     [TestProperty("Author", "Diego Rojas")]
     public void TCID1()
     {
+        string stringToSearch = "blouse";
+
         HomePage homePage = new HomePage(Driver);
         homePage.GoTo();
-        SearchPage searchPage = homePage.Search("blouse");
-        Assert.IsTrue(searchPage.Contains(Item.Blouse));
+        SearchPage searchPage = homePage.Search(stringToSearch);
+        Assert.IsTrue(searchPage.Contains(Item.Blouse), 
+            $"When searching for the string=>{stringToSearch}, " +
+            $"we did not find it in the search results.");
     }
 
     [TestInitialize]
