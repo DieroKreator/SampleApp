b304c4a8f37783419ba4da80b771821b75ee633e
DieroKreator <qtechwired@gmail.com>
2025-02-05 08:25:22 -0300

    Add BaseApplicationPage class

---
+++ Pages/BaseApplicationPage.cs
@@ -0,0 +1,13 @@
+using OpenQA.Selenium;
+
+namespace AutomationPractice;
+
+public class BaseApplicationPage
+{
+    protected IWebDriver Driver { get; set; }
+
+    public BaseApplicationPage(IWebDriver driver)
+    {
+        Driver = driver;
+    }
+}
