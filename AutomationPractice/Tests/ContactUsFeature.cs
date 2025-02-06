fe0f28e92a9d054444572000665fe512197461ee
DieroKreator <qtechwired@gmail.com>
2025-02-05 21:16:33 -0300

    remove redundant driver setup and cleanup methods

--- AutomationPractice/Tests/ContactUsFeature.cs
+++ AutomationPractice/Tests/ContactUsFeature.cs
@@ -1,38 +1,16 @@
-using OpenQA.Selenium;
-
 namespace AutomationPractice;
 
-public class ContactUsFeature
+[TestClass]
+[TestCategory("ContactUsPage")]
+public class ContactUsFeature : BaseTest
 {
-    public IWebDriver? Driver { get; set; }
-
     [TestMethod]
-    [Description("")]
+    [Description("Validate that the contact us page opens successfully with a form.")]
     [TestProperty("Author", "Diego Rojas")]
     public void TCID2()
     {
         ContactUsPage contactUsPage = new ContactUsPage(Driver);
         contactUsPage.GoTo();
         Assert.IsTrue(contactUsPage.IsLoaded, "The contact us page did not open successfully");
-    }
-
-    [TestInitialize]
-    public void SetupForEverySingleTestMethod()
-    {
-        Driver = GetChromeDriver();
-    }
-
-    [TestCleanup]
-    public void CleanUpAfterEveryTestMethod()
-    {
-        Driver.Close();
-        Driver.Quit();
-    }
-
-    private IWebDriver GetChromeDriver()
-    {
-        var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        return new ChromeDriver(outPutDirectory);
-        // return new ChromeDriver();
     }
 }
