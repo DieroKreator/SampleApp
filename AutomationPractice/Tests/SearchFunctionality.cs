464423eef6dcd9ae926ee84b984b1a033471d3b9
DieroKreator <qtechwired@gmail.com>
2025-02-05 21:18:17 -0300

    remove redundant driver setup methods

--- AutomationPractice/Tests/SearchFunctionality.cs
+++ AutomationPractice/Tests/SearchFunctionality.cs
@@ -1,15 +1,9 @@
-using System.Reflection;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-
 namespace AutomationPractice;
 
 [TestClass]
 [TestCategory("SearchingFeature")]
-public class SearchFunctionality
+public class SearchFunctionality : BaseTest
 {
-    public IWebDriver? Driver { get; set; }
-
     [TestMethod]
     [Description("Checks to make sure that if we search for browser, that browser comes back")]
     [TestProperty("Author", "Diego Rojas")]
@@ -23,25 +17,5 @@ public class SearchFunctionality
         Assert.IsTrue(searchPage.Contains(Item.Blouse), 
             $"When searching for the string=>{stringToSearch}, " +
             $"we did not find it in the search results.");
-    }
-
-    [TestInitialize]
-    public void SetupForEverySingleTestMethod()
-    {
-        Driver = GetChromeDriver();
-    }
-
-    [TestCleanup]
-    public void CleanUpAfterEveryTestMethod()
-    {
-        Driver.Close();
-        Driver.Quit();
-    }
-
-    private IWebDriver GetChromeDriver()
-    {
-        var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        return new ChromeDriver(outPutDirectory);
-        // return new ChromeDriver();
     }
 }
