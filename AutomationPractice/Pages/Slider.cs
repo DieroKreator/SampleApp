9f79facd7929fb32b083b558789b1444a5bba487
DieroKreator <qtechwired@gmail.com>
2025-02-06 23:54:08 -0300

    Refactor Slider class to improve image access and implement ClickNextButton method

--- AutomationPractice/Pages/Slider.cs
+++ AutomationPractice/Pages/Slider.cs
@@ -5,16 +5,15 @@ namespace AutomationPractice;
 public class Slider : BaseApplicationPage
 {
     private IWebDriver driver;
-    
-    public Slider(IWebDriver driver) : base(driver)
-    {
-        this.driver = driver;
-    }
 
-    public object CurrentImage => Driver.FindElement(By.Id("homeslider")).GetAttribute("style");
+    public Slider(IWebDriver driver) : base(driver) { }
+
+    public object CurrentImage => MainSliderImage.GetAttribute("style");
+
+    private IWebElement MainSliderImage => Driver.FindElement(By.Id("homeslider"));
 
     internal void ClickNextButton()
     {
-        throw new NotImplementedException();
+        Driver.FindElement(By.ClassName("bx-next")).Click();
     }
 }
